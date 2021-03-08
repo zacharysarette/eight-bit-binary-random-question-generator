fc649c76d13bd0504a46fba4782ec2e5aa0eecc1
zacharysarette <zacharywsarette@gmail.com>
2021-03-08 23:15:07 +0900

    initial commit

---
+++ binaryQuizGen.js
@@ -0,0 +1,24 @@
+const binToDecArr = [128, 64, 32, 16, 8, 4, 2, 1];
+
+const getRandom8bit = () => {
+  const arr = [];
+  for (let i = 8; i > 0; i--) {
+    arr.push(Math.random() > 0.5 ? "1" : "0");
+  }
+  return arr;
+};
+
+const generateBinToDecQuestion = (binStrArr) => {
+  return "What is " + binStrArr.join("") + " in decimal?";
+};
+
+const getBinToDecAnswer = (binStrArr = []) => {
+  return binStrArr
+    .map((e, i) => (e === "1" ? binToDecArr[i] : 0))
+    .reduce((a, b) => a + b);
+};
+
+const generateQuestionAndAnswer = (r = getRandom8bit()) => ({
+  q: generateBinToDecQuestion(r),
+  a: getBinToDecAnswer(r),
+});
