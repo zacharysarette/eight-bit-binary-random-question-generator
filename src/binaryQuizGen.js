d203ef8f28d358140a3d92c0d77a5617c38efe13
zacharysarette <zacharywsarette@gmail.com>
2021-03-08 23:32:56 +0900

    encapsulates  into a single function

--- src/binaryQuizGen.js
+++ src/binaryQuizGen.js
@@ -1,24 +1,28 @@
-const binToDecArr = [128, 64, 32, 16, 8, 4, 2, 1]
+const generateBinaryToDecimalQuestionAndAnswer = () => {
+  const binToDecArr = [128, 64, 32, 16, 8, 4, 2, 1]
 
-const getRandom8bit = () => {
-  const arr = []
-  for (let i = 8; i > 0; i--) {
-    arr.push(Math.random() > 0.5 ? '1' : '0')
+  const getRandom8bit = () => {
+    const arr = []
+    for (let i = 8; i > 0; i--) {
+      arr.push(Math.random() > 0.5 ? '1' : '0')
+    }
+    return arr
   }
-  return arr
+
+  const generateBinToDecQuestion = (binStrArr) =>
+    'What is ' + binStrArr.join('') + ' in decimal?'
+
+  const getBinToDecAnswer = (binStrArr = []) =>
+    binStrArr
+      .map((e, i) => (e === '1' ? binToDecArr[i] : 0))
+      .reduce((a, b) => a + b)
+
+  const generateQuestionAndAnswer = (r = getRandom8bit()) => ({
+    q: generateBinToDecQuestion(r),
+    a: getBinToDecAnswer(r)
+  })
+
+  return generateQuestionAndAnswer()
 }
 
-const generateBinToDecQuestion = (binStrArr) =>
-  'What is ' + binStrArr.join('') + ' in decimal?'
-
-const getBinToDecAnswer = (binStrArr = []) =>
-  binStrArr
-    .map((e, i) => (e === '1' ? binToDecArr[i] : 0))
-    .reduce((a, b) => a + b)
-
-const generateQuestionAndAnswer = (r = getRandom8bit()) => ({
-  q: generateBinToDecQuestion(r),
-  a: getBinToDecAnswer(r)
-})
-
-export default generateQuestionAndAnswer
+export default generateBinaryToDecimalQuestionAndAnswer
