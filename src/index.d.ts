2bd2ee28a2a20715587e4f156a43db46b2b46fd1
zacharysarette <zacharywsarette@gmail.com>
2021-03-09 22:01:15 +0900

    Fixing types

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,9 +3,9 @@ export default generateQuestionAndAnswer;
 declare function generateQuestionAndAnswer(type?: string, isMultipleChoice?: boolean): {
     q: string;
     a: number;
-    multipleChoice: undefined | {a:number, b:number, c:number, e:number, f:number}
+    multipleChoice: undefined | {a:number, b:number, c:number, d:number, e:number, f:number}
 } | {
     q: string;
     a: string;
-    multipleChoice: undefined | {a:string, b:string, c:string, e:string, f:string} 
+    multipleChoice: undefined | {a:string, b:string, c:string, d:string, e:string, f:string} 
 };
