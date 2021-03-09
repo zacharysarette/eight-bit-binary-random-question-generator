1acfaab19f9e10c3059f7e2b449fac0dfe03d857
zacharysarette <zacharywsarette@gmail.com>
2021-03-09 22:47:49 +0900

    Udpates function to match usage from the readme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import shuffle from './shuffle'
 import getBins from './const/getBins'
 
-const generateQuestionAndAnswer = (
-  type = 'binToDec',
-  isMultipleChoice = true
-) => {
+const generateQuestionAndAnswer = (type = '', isMultipleChoice = true) => {
   const binToDecArr = [128, 64, 32, 16, 8, 4, 2, 1]
 
   const getRandom8bit = () => {
@@ -29,8 +26,7 @@ const generateQuestionAndAnswer = (
   const generateBinToDecQuestionAndAnswer = (r = getRandom8bit()) => ({
     q: generateBinToDecQuestion(r),
     a: getBinToDec(r),
-    multipleChoice:
-      isMultipleChoice && getBinMultiChoice(r.join(''), true)
+    multipleChoice: isMultipleChoice && getBinMultiChoice(r.join(''), true)
   })
 
   const generateDecToBinQuestionAndAnswer = (r = getRandom8bit()) => ({
@@ -39,7 +35,7 @@ const generateQuestionAndAnswer = (
     multipleChoice: isMultipleChoice && getBinMultiChoice(r.join(''))
   })
 
-  const getBinMultiChoice = (bin="00000001", isDec = false) => {
+  const getBinMultiChoice = (bin = '00000001', isDec = false) => {
     const shuffled = shuffle(getBins())
 
     const answers = shuffle([
@@ -52,23 +48,22 @@ const generateQuestionAndAnswer = (
       shuffled[5]
     ])
 
-    return ({
-      a: isDec ? getBinToDec(answers[0].split("")): answers[0],
-      b: isDec ? getBinToDec(answers[1].split("")): answers[1],
-      c: isDec ? getBinToDec(answers[2].split("")): answers[2],
-      d: isDec ? getBinToDec(answers[3].split("")): answers[3],
-      e: isDec ? getBinToDec(answers[4].split("")): answers[4],
-      f: isDec ? getBinToDec(answers[5].split("")): answers[5],
-    })
+    return {
+      a: isDec ? getBinToDec(answers[0].split('')) : answers[0],
+      b: isDec ? getBinToDec(answers[1].split('')) : answers[1],
+      c: isDec ? getBinToDec(answers[2].split('')) : answers[2],
+      d: isDec ? getBinToDec(answers[3].split('')) : answers[3],
+      e: isDec ? getBinToDec(answers[4].split('')) : answers[4],
+      f: isDec ? getBinToDec(answers[5].split('')) : answers[5]
+    }
   }
+  const getRandBool = () => Math.random() > 0.5
 
-  return type === 'binToDec'
-    ? generateBinToDecQuestionAndAnswer()
-    : type === 'decToBin'
-      ? generateDecToBinQuestionAndAnswer()
-      : Math.random() < 0.5
-        ? generateBinToDecQuestionAndAnswer()
-        : generateDecToBinQuestionAndAnswer()
+  if (type === 'decToBin') return generateDecToBinQuestionAndAnswer()
+  if (type === 'binToDec') return generateBinToDecQuestionAndAnswer()
+  return getRandBool()
+    ? generateDecToBinQuestionAndAnswer()
+    : generateBinToDecQuestionAndAnswer()
 }
 
 export default generateQuestionAndAnswer
